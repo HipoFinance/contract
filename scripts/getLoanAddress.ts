09f45895e13a4f85c9378c27b657873574579118
Behrang Norouzinia <behrangn@gmail.com>
2023-07-25 15:56:11 +0330

    Add scripts for participation

---
+++ scripts/getLoanAddress.ts
@@ -0,0 +1,23 @@
+import { Address, beginCell, Cell, Dictionary } from 'ton-core'
+import { Treasury } from '../wrappers/Treasury'
+import { NetworkProvider } from '@ton-community/blueprint'
+import { sha256_sync } from 'ton-crypto'
+
+export async function run(provider: NetworkProvider) {
+    const ui = provider.ui();
+
+    const addressString = await ui.input('Enter the friendly address of the treasury')
+    const treasuryAddress = Address.parse(addressString)
+    const treasury = provider.open(Treasury.createFromAddress(treasuryAddress))
+
+    const roundSince = BigInt(await ui.input('participation round'))
+    const validatorAddress = provider.sender().address
+    if (validatorAddress == null) {
+        return
+    }
+
+    const loanAddress = await treasury.getLoanAddress(validatorAddress, roundSince)
+    console.log('Loan Address:\n  Raw: %s\n  Friendly: %s\n', loanAddress.toRawString(), loanAddress.toString({ urlSafe: true, bounceable: true, testOnly: true }))
+
+    ui.write('Done');
+}
--- scripts/requestLoan.ts
+++ scripts/requestLoan.ts
@@ -12,11 +12,11 @@ export async function run(provider: NetworkProvider) {
     const roundSince = BigInt(await ui.input('participation round'))
     const value = toNano(await ui.input('value'))
     const loanAmount = toNano(await ui.input('loan amount'))
-    const minPayment = toNano(await ui.input('min payment'))
+    const minPayment = toNano(await ui.input('min payment (default: 0)') || '0')
     const validatorRewardShare = BigInt(await ui.input('validator reward share [0-255] (default: 102)') || '102')
-    const validatorPubkey = BigInt('0x' + await ui.input('validator pubkey'))
-    const maxFactor = BigInt(await ui.input('max factor'))
+    const maxFactor = BigInt(await ui.input('max factor (default: 65536)') || '65536')
     const adnlAddress = BigInt('0x' + await ui.input('adnl address'))
+    const validatorPubkey = BigInt('0x' + await ui.input('validator pubkey'))
     const signature = BigInt('0x' + await ui.input('signature'))
 
     await treasury.sendRequestLoan(provider.sender(), {
