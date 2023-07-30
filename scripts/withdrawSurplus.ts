01ad40d46fa432680cf2df1be7da961dab64c681
Behrang Norouzinia <behrangn@gmail.com>
2023-07-30 16:03:00 +0330

    Add script to withdraw surplus of treasury

---
+++ scripts/withdrawSurplus.ts
@@ -0,0 +1,17 @@
+import { Address } from 'ton-core'
+import { Treasury } from '../wrappers/Treasury'
+import { NetworkProvider } from '@ton-community/blueprint'
+
+export async function run(provider: NetworkProvider) {
+    const ui = provider.ui();
+
+    console.log('Withdraw surplus of treasury')
+
+    const addressString = await ui.input('Enter the friendly address of the treasury')
+    const treasuryAddress = Address.parse(addressString)
+    const treasury = provider.open(Treasury.createFromAddress(treasuryAddress))
+
+    await treasury.sendWithdrawSurplus(provider.sender(), { value: '0.1' })
+
+    ui.write('Done');
+}
