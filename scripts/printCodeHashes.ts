62f579092cfa73ea185f8e69c97e305b5ab0bc06
Behrang Norouzinia <behrangn@gmail.com>
2023-10-29 18:34:33 +0330

    Add script for printing code hashes

---
+++ scripts/printCodeHashes.ts
@@ -0,0 +1,25 @@
+import { compile } from '@ton-community/blueprint'
+
+export async function run() {
+    const treasuryCode = await compile('Treasury')
+    const walletCode = await compile('Wallet')
+    const loanCode = await compile('Loan')
+
+    const treasuryCodeHash = treasuryCode.hash()
+    const walletCodeHash = walletCode.hash()
+    const loanCodeHash = loanCode.hash()
+
+    console.info()
+
+    console.info(`treasury code hash hex:    ${treasuryCodeHash.toString('hex')}`)
+    console.info(`treasury code hash base64: ${treasuryCodeHash.toString('base64')}`)
+    console.info()
+
+    console.info(`wallet code hash hex:    ${walletCodeHash.toString('hex')}`)
+    console.info(`wallet code hash base64: ${walletCodeHash.toString('base64')}`)
+    console.info()
+
+    console.info(`loan code hash hex:    ${loanCodeHash.toString('hex')}`)
+    console.info(`loan code hash base64: ${loanCodeHash.toString('base64')}`)
+    console.info()
+}
