50eff52481dacb22d56862b3aa54f088944b4adb
Behrang Norouzinia <behrangn@gmail.com>
2024-06-18 10:42:38 +0330

    Update jest config to increase timeout

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     setupFilesAfterEnv: ['<rootDir>/tests/setup-jest.ts'],
     slowTestThreshold: 60,
+    testTimeout: 10000,
     workerThreads: true,
 }
 
